b0bd9c89b0b2fe351e8a88f00a8a18ad55cddfeb
apm30 <m.ajaypradeep@icloud.com>
2023-02-27 23:28:56 +0000

    Adding java docs for model and tests.

--- src/main/java/com/stacs/routemappingapp/model/stop/Stop.java
+++ src/main/java/com/stacs/routemappingapp/model/stop/Stop.java
@@ -4,6 +4,28 @@ import java.io.Serializable;
 
 /**
  * Model class for Stop.
+ * This class is used to store the information of a stop.
+ * The class implements Serializable to allow the object to be passed between activities.
+ * The class has the following attributes:
+ * RouteNumber: The route number of the route that the stop is on.
+ * RouteName: The name of the route that the stop is on.
+ * scheduleIdentifier: The schedule identifier of the stop.
+ * stopName: The name of the stop.
+ * stopLocation: The location of the stop.
+ * date: The date of the stop.
+ * day: The day of the stop.
+ * departureTime: The departure time of the stop.
+ * arrivalTime: The arrival time of the stop.
+ * The class has the following methods:
+ * getRouteNumber: Returns the route number of the route that the stop is on.
+ * getRouteName: Returns the name of the route that the stop is on.
+ * getScheduleIdentifier: Returns the schedule identifier of the stop.
+ * getStopName: Returns the name of the stop.
+ * getStopLocation: Returns the location of the stop.
+ * getDate: Returns the date of the stop.
+ * getDay: Returns the day of the stop.
+ * getDepartureTime: Returns the departure time of the stop.
+ * getArrivalTime: Returns the arrival time of the stop.
  */
 public class Stop implements Serializable {
     private final String RouteNumber;
--- src/test/java/com/stacs/routemappingapp/model/route/RouteTest.java
+++ src/test/java/com/stacs/routemappingapp/model/route/RouteTest.java
@@ -2,26 +2,41 @@ package com.stacs.routemappingapp.model.route;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+/**
+ * Test class for Route
+ */
 class RouteTest {
     Route route = new Route("N64", "NorthRoad", "StAndrews", "Dundee");
 
+    /**
+     * Test for getUniqueRouteNumber
+     */
     @Test
     void getUniqueRouteNumber() {
         assertEquals(route.getUniqueRouteNumber(), "N64");
     }
 
+    /**
+     * Test for getRouteName
+     */
     @Test
     void getRouteName() {
         assertEquals(route.getRouteName(), "NorthRoad");
     }
 
+    /**
+     * Test for getDestination
+     */
     @Test
     void getDestination() {
         assertEquals(route.getDestination(), "StAndrews");
     }
 
+    /**
+     * Test for getStartingPoint
+     */
     @Test
     void getStartingPoint() {
         assertEquals(route.getStartingPoint(), "Dundee");
