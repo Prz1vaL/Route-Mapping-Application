9d92c92a7a0aa6bbb22e35e246db205ff7c9c3c2
apm30 <m.ajaypradeep@icloud.com>
2023-02-27 12:57:38 +0000

    Maven and SpringBoot initial.

---
+++ src/main/java/com/stacs/routemappingapp/RouteMappingAppApplication.java
@@ -0,0 +1,15 @@
+package com.stacs.routemappingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RouteMappingAppApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(RouteMappingAppApplication.class, args);
+
+	}
+
+}
---
+++ src/test/java/com/stacs/routemappingapp/RouteMappingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stacs.routemappingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RouteMappingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
