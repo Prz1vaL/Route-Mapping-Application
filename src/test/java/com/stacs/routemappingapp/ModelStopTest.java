89495edd57445f88e86a46e05fa7ee10719aabff
apm30 <apm30@st-andrews.ac.uk>
2023-02-27 22:42:15 +0000

    Update ModelStopTest.java

--- src/test/java/com/stacs/routemappingapp/ModelStopTest.java
+++ src/test/java/com/stacs/routemappingapp/ModelStopTest.java
@@ -10,14 +10,14 @@ public class ModelStopTest {
 	public void shouldCreateStop() {
 		Stop stop = new Stop("N64", "NorthRoad", "ID", "Cardinal", "Dundee", "27/02/2023", "Monday", "13:00", "16:00");
 
-        assertEqual(stop.getRouteNumber(), "N64");
-        assertEqual(stop.getRouteName(), "NorthRoad");
-        assertEqual(stop.getScheduleIdentifier(), "ID");
-        assertEqual(stop.getStopName(), "Cardinal");
-        assertEqual(stop.getStopLocation(), "Dundee");
-        assertEqual(stop.getDate(), "27/02/2023");
-        assertEqual(stop.getDay(), "Monday");
-        assertEqual(stop.getDepartureTime(), "13:00");
-        assertEqual(stop.getArrivalTime(), "16:00");
+        assertEquals(stop.getRouteNumber(), "N64");
+        assertEquals(stop.getRouteName(), "NorthRoad");
+        assertEquals(stop.getScheduleIdentifier(), "ID");
+        assertEquals(stop.getStopName(), "Cardinal");
+        assertEquals(stop.getStopLocation(), "Dundee");
+        assertEquals(stop.getDate(), "27/02/2023");
+        assertEquals(stop.getDay(), "Monday");
+        assertEquals(stop.getDepartureTime(), "13:00");
+        assertEquals(stop.getArrivalTime(), "16:00");
 	}
 }
