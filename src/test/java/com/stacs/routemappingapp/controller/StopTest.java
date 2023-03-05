0d0b688984d5449df03265d8bb61dcdc49ae2798
220030476 <glad1@st-andrews.ac.uk>
2023-03-05 17:10:17 +0000

    added test for ifStopExistsbyTime in controller

--- src/test/java/com/stacs/routemappingapp/controller/StopTest.java
+++ src/test/java/com/stacs/routemappingapp/controller/StopTest.java
@@ -44,6 +44,9 @@ class StopTest {
         mockMvc = MockMvcBuilders.standaloneSetup(stopController).build();
     }
 
+    /*
+     * Test to get all stops
+     */
     @Test
     void shouldGetAllStops() throws Exception {
         Stop stop1 = new Stop("uniqueRouteNumber1", "routeName1", "scheduleIdentifier1", "stopName1", 
@@ -53,6 +56,7 @@ class StopTest {
         List<Stop> stops = Arrays.asList(stop1, stop2);
         when(stopService.getStops()).thenReturn(stops);
 
+        // Perform the GET request
         mockMvc.perform(MockMvcRequestBuilders.get("/stops")
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(MockMvcResultMatchers.status().isOk())
@@ -78,6 +82,9 @@ class StopTest {
                 .andDo(MockMvcResultHandlers.print());
     }
 
+    /*
+     * Test to add Stop 
+     */
     @Test
     void shouldAddStops() throws Exception {
         Map<String, String> data = new HashMap<>();
@@ -117,12 +124,15 @@ class StopTest {
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andDo(MockMvcResultHandlers.print());
 
-        // Verify that the deleteRoute method was called with the correct arguments
+        // Verify that the deleteStop method was called with the correct arguments
         verify(stopService, times(1)).deleteStop("scheduleIdentifier1");
     }
 
+    /*
+     * Test to view all routes by stop name
+     */
     @Test
-    void testViewRoutesByStop() throws Exception {
+    void shouldViewRoutesByStop() throws Exception {
         HashMap<String, Stop> routesByStop = new HashMap<>();
         Stop stop1 = new Stop("uniqueRouteNumber1", "routeName1", "scheduleIdentifier1", "stopName1", 
             "stopLocation1", "date1", "day1", "departureTime1", "arrivalTime1");
@@ -133,6 +143,7 @@ class StopTest {
 
         when(stopService.viewRoutesByStop("stopName1")).thenReturn(routesByStop);
 
+        // Perform the GET request
         mockMvc.perform(MockMvcRequestBuilders.get("/stops/" + "stopName1" + "/routes")
             .contentType(MediaType.APPLICATION_JSON))
             .andExpect(MockMvcResultMatchers.status().isOk())
@@ -146,11 +157,15 @@ class StopTest {
             .andExpect(MockMvcResultMatchers.jsonPath("$[*].departureTime", hasItems("departureTime1", "departureTime2")))
             .andExpect(MockMvcResultMatchers.jsonPath("$[*].arrivalTime", hasItems("arrivalTime1", "arrivalTime2")));
 
+        // Verify that the viewRoutesByStop method was called with the correct arguments
         verify(stopService, times(1)).viewRoutesByStop("stopName1");
     }
 
+    /*
+     * Test to view all routes by name and day
+     */
     @Test
-    void testViewRoutesByStopDay() throws Exception {
+    void shouldViewRoutesByStopDay() throws Exception {
         HashMap<String, Stop> routesByDay = new HashMap<>();
         Stop stop1 = new Stop("uniqueRouteNumber1", "routeName1", "scheduleIdentifier1", "stopName1", 
             "stopLocation1", "date1", "day1", "departureTime1", "arrivalTime1");
@@ -161,10 +176,11 @@ class StopTest {
 
         when(stopService.viewRoutesByStopDay("stopName1", "day1")).thenReturn(routesByDay);
 
+        // Perform the GET request
         mockMvc.perform(MockMvcRequestBuilders.get("/stops/" + "stopName1" + '/' + "day1" + "/routes")
             .contentType(MediaType.APPLICATION_JSON))
             .andExpect(MockMvcResultMatchers.status().isOk())
-            .andExpect(MockMvcResultMatchers.jsonPath("$[*].uniqueRouteNumber", hasItems("uniqueRouteNumber1", "uniqueRouteNumber2")))
+            .andExpect(MockMvcResultMatchers.jsonPath("$[*].routeNumber", hasItems("uniqueRouteNumber1", "uniqueRouteNumber2")))
             .andExpect(MockMvcResultMatchers.jsonPath("$[*].routeName", hasItems("routeName1", "routeName2")))
             .andExpect(MockMvcResultMatchers.jsonPath("$[*].scheduleIdentifier", hasItems("scheduleIdentifier1", "scheduleIdentifier2")))
             .andExpect(MockMvcResultMatchers.jsonPath("$[*].stopName", hasItems("stopName1", "stopName1")))
@@ -174,6 +190,41 @@ class StopTest {
             .andExpect(MockMvcResultMatchers.jsonPath("$[*].departureTime", hasItems("departureTime1", "departureTime2")))
             .andExpect(MockMvcResultMatchers.jsonPath("$[*].arrivalTime", hasItems("arrivalTime1", "arrivalTime2")));
 
+        // Verify that the viewRoutesByStopDay method was called with the correct arguments
         verify(stopService, times(1)).viewRoutesByStopDay("stopName1", "day1");
+    }
+
+    /*
+     * Test to get all routes with a specified stop name 
+     * and that exist at the specified time 
+     */
+    @Test
+    void shouldGetIfStopExistsbyTime() throws Exception {
+        HashMap<String, Stop> routesByTime = new HashMap<>();
+        Stop stop1 = new Stop("uniqueRouteNumber1", "routeName1", "scheduleIdentifier1", "stopName1", 
+            "stopLocation1", "date1", "day1", "13:00", "16:00");
+        Stop stop2 = new Stop("uniqueRouteNumber2", "routeName2", "scheduleIdentifier2", "stopName1",
+            "stopLocation2", "date2", "day2", "12:00", "14:00");
+        routesByTime.put("scheduleIdentifier1", stop1);
+        routesByTime.put("scheduleIdentifier2", stop2);
+
+        when(stopService.ifStopExistsbyTime("stopName1", "13:00")).thenReturn(routesByTime);
+
+        // Perform the GET request
+        mockMvc.perform(MockMvcRequestBuilders.get("/stops" + "/routes" + '/' + "stopName1" + '/' + "13:00")
+            .contentType(MediaType.APPLICATION_JSON))
+            .andExpect(MockMvcResultMatchers.status().isOk())
+            .andExpect(MockMvcResultMatchers.jsonPath("$[*].uniqueRouteNumber1", hasItems("uniqueRouteNumber1", "uniqueRouteNumber2")))
+            .andExpect(MockMvcResultMatchers.jsonPath("$[*].routeName", hasItems("routeName1", "routeName2")))
+            .andExpect(MockMvcResultMatchers.jsonPath("$[*].scheduleIdentifier", hasItems("scheduleIdentifier1", "scheduleIdentifier2")))
+            .andExpect(MockMvcResultMatchers.jsonPath("$[*].stopName", hasItems("stopName1", "stopName1")))
+            .andExpect(MockMvcResultMatchers.jsonPath("$[*].stopLocation", hasItems("stopLocation1", "stopLocation2")))
+            .andExpect(MockMvcResultMatchers.jsonPath("$[*].date", hasItems("date1", "date2")))
+            .andExpect(MockMvcResultMatchers.jsonPath("$[*].day", hasItems("day1", "day2")))
+            .andExpect(MockMvcResultMatchers.jsonPath("$[*].departureTime", hasItems("13:00", "12:00")))
+            .andExpect(MockMvcResultMatchers.jsonPath("$[*].arrivalTime", hasItems("16:00", "14:00")));
+
+        // Verify that the ifStopExistsbyTime method was called with the correct arguments
+        verify(stopService, times(1)).ifStopExistsbyTime("stopName1", "13:00");
     }
 }
