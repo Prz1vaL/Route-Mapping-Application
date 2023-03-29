9dd145ae60f6ff8058989813a62a5e7ec58b78ad
Ajay Pradeep Mahadeven <m.ajaypradeep@icloud.com>
2023-03-29 08:47:43 +0100

    Update MainApp.java
    code line error

--- src/main/java/com/stacs/routemappingapp/MainApp.java
+++ src/main/java/com/stacs/routemappingapp/MainApp.java
@@ -11,12 +11,8 @@ import com.stacs.routemappingapp.view.CommandLine;
 public class MainApp {
 
     public static void main(String[] args) {
-        // Instances of Service Layer
+         // Instance of Controller Layer
         RoutingResource routingService = new RoutingResource();
-
-        // Instance of Controller Layer
-        RoutingResource routingResource = new RoutingResource();
-        StopService stopService = new StopService();
 
         // Terminal View Method is called
         System.out.println("Welcome to the Route Mapping App!");
